f4de6ccb1769743e8abae33bca1d8ed65fcb03f2
Eduardo Medeiros <eduardoamedeiros22@gmail.com>
2022-09-21 00:09:07 -0300

    Consecutive Mining

--- data_organize/consecutive_mining.py
+++ data_organize/consecutive_mining.py
@@ -1,28 +1,68 @@
+import random
+
 from db_connection.connection import get_database
 
 
 def main():
+    # primary_block_ordenation_consecutive_count()
+    permutation_block_ordenation_consecutive_count(10)
+
+
+def permutation_block_ordenation_consecutive_count(number_of_permutations):
     dbname = get_database()
-    collection_miner = dbname["miner"]
+    collection_blocks = dbname["blocos"]
     collection_consecutive = dbname["consecutive_mining"]
-    mineradores = list(collection_miner.find())
-    for minerador in mineradores:
-        structure_consecutive_miner = {}
-        block_list = minerador['blocks']
-        previous_block = 0
-        consecutive = 0
-        max_consecutive = 0
-        for block in block_list:
-            if block == previous_block + 1:
-                consecutive += 1
-                if consecutive > max_consecutive:
-                    max_consecutive = consecutive
-            else:
-                consecutive = 1
-            previous_block = block
-        structure_consecutive_miner['miner'] = minerador['address']
-        structure_consecutive_miner['consecutive_mining'] = max_consecutive
-        collection_consecutive.insert_one(structure_consecutive_miner)
+    blocos = list(collection_blocks.find())
+    miners = list(collection_consecutive.find())
+
+    previous_block = {}
+    structure_consecutive_miner_list = []
+
+    while number_of_permutations > 0:
+        random.shuffle(blocos)
+        for miner in miners:
+            repetition_count = 0
+            for bloco in blocos:
+                if previous_block:
+                    if bloco['miner_adress'] == miner['miner']:
+                        if bloco['miner_adress'] == previous_block['miner_adress']:
+                            repetition_count += 1
+                previous_block = bloco
+            miner['consecutive_mining'].append(repetition_count)
+            structure_consecutive_miner_list.append(dict(miner))
+
+        for miner_dict in structure_consecutive_miner_list:
+            collection_consecutive.replace_one({'miner': miner_dict['miner']}, dict(miner_dict))
+        number_of_permutations -= 1
+
+
+def primary_block_ordenation_consecutive_count():
+    dbname = get_database()
+    collection_blocks = dbname["blocos"]
+    collection_consecutive = dbname["consecutive_mining"]
+    collection_miner = dbname["miner"]
+    blocos = list(collection_blocks.find())
+    miners = list(collection_miner.find())
+
+    previous_block = {}
+    structure_consecutive_miner = {}
+    structure_consecutive_miner_list = []
+
+    for miner in miners:
+        repetition_count = 0
+        structure_consecutive_miner['miner'] = miner['address']
+        structure_consecutive_miner['consecutive_mining'] = []
+        for bloco in blocos:
+            if previous_block:
+                if bloco['miner_adress'] == structure_consecutive_miner['miner']:
+                    if bloco['miner_adress'] == previous_block['miner_adress']:
+                        repetition_count += 1
+            previous_block = bloco
+        structure_consecutive_miner['consecutive_mining'].append(repetition_count)
+        structure_consecutive_miner_list.append(dict(structure_consecutive_miner))
+
+    for structure_consecutive_miner_dict in structure_consecutive_miner_list:
+        collection_consecutive.insert_one(dict(structure_consecutive_miner_dict))
 
 
 if __name__ == "__main__":
