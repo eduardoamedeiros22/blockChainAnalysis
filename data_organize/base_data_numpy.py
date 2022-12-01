ada0977be342d573d06120ed2e340e491237abcd
Eduardo Medeiros <eduardoamedeiros22@gmail.com>
2022-12-01 00:24:25 -0300

    Adição de nova analise com integers no lugar de hashs

--- data_organize/base_data_numpy.py
+++ data_organize/base_data_numpy.py
@@ -5,6 +5,7 @@ import numpy as np
 def main():
     npy_block_array_generate()
     npy_miner_array_generate()
+    npy_block_array_generate_with_index()
 
 
 def npy_block_array_generate():
@@ -20,6 +21,20 @@ def npy_block_array_generate():
 
     np.save(path_block_array, block_array, allow_pickle=True)
     f.close()
+
+
+def npy_block_array_generate_with_index():
+    block_array = np.load('../resources/block_array.npy')
+    miner_array = np.load('../resources/miner_array.npy')
+    miner_array_with_index = list(miner_array)
+    block_integer_array_np = ('../resources/block_integer_array')
+    block_integer_array = []
+
+    for block in block_array:
+        block_integer_array.append(miner_array_with_index.index(block))
+
+    np.save(block_integer_array_np, block_integer_array, allow_pickle=True)
+
 
 def npy_miner_array_generate():
     path_miner = '../resources/miner.json'
--- data_organize/consecutive_mining.py
+++ data_organize/consecutive_mining.py
@@ -7,11 +7,13 @@ import numpy as np
 def main():
     ## Busca os dados de mineradores e blocos ##
     ## Vetor de blocos apenas com o hash dos mineradores ##
-    block_list = np.load('../resources/block_array.npy')
-    miner_list = np.load('../resources/miner_array.npy')
+    block_list = list(np.load('../resources/block_array.npy'))
+    miner_list = list(np.load('../resources/miner_array.npy'))
+    integer_block_list = list(np.load('../resources/block_integer_array.npy'))
 
     ini = time.time()
-    permutation_block_ordenation_consecutive_count(1000, 1000, 30000, block_list, miner_list)
+    # permutation_block_ordenation_consecutive_count(1000, 1000, 30000, block_list, miner_list)
+    permutation_block_integer_ordenation_consecutive_count(1000, 1000, 30000, integer_block_list, miner_list)
     fim = time.time()
     print("Tempo em segundos: ", fim - ini)
 
@@ -34,6 +36,40 @@ def permutation_block_ordenation_consecutive_count(number_of_permutations, k_val
             previous_block = "none"
             for bloco in block_interval:
                 if bloco == miner:
+                    if bloco == previous_block:
+                        repetition_count += 1
+                previous_block = bloco
+            consecutive_mining_count_list.append(repetition_count)
+            permutations -= 1
+            random.shuffle(block_interval)
+
+        # Salva a estrutura por minerador
+        consecutive_mining_structure['miner'] = miner
+        consecutive_mining_structure['consecutive_analysis'] = consecutive_mining_count_list
+        structure_consecutive_miner_list.append(consecutive_mining_structure)
+
+    print(structure_consecutive_miner_list)
+
+
+def permutation_block_integer_ordenation_consecutive_count(number_of_permutations, k_value, initial_block, blocks, miners):
+
+    structure_consecutive_miner_list = []
+    miners_list = list(miners)
+
+    for miner in miners:
+
+        # Zerando os valores de parametro
+        consecutive_mining_structure = {}
+        consecutive_mining_count_list = []
+        block_interval = blocks[initial_block: initial_block + k_value]
+        permutations = number_of_permutations
+
+        # Iniciando as permutações
+        while permutations > 0:
+            repetition_count = 0
+            previous_block = -1
+            for bloco in block_interval:
+                if bloco == miners_list.index(miner):
                     if bloco == previous_block:
                         repetition_count += 1
                 previous_block = bloco
