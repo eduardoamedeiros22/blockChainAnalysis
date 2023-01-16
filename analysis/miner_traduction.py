582571bce218e46b7b5128086bf1e326b619367d
Eduardo Medeiros <eduardoamedeiros22@gmail.com>
2023-01-16 19:06:20 -0300

    Dados para tabela

---
+++ analysis/miner_traduction.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def main():
+    miner_list = list(np.load('../resources/miner_array.npy'))
+    miner_traduction_table(miner_list)
+
+
+def miner_traduction_table(miner_list):
+    for miner in miner_list:
+        print("{} & {}".format(miner_list.index(miner), miner))
+
+
+if __name__ == "__main__":
+    main()
--- result_creation/new_plots.py
+++ result_creation/new_plots.py
@@ -26,28 +26,25 @@ def main():
 
     x_height_middle = []
     y_hash_midle= []
-    print(block_list[45001])
-    # for block in block_list:
-    #     x_height_total.append(block_list.index(block))
-    #     y_hash_total.append(block)
-    #     if 45000 < contador < 50000:
-    #         x_height_middle.append(block_list.index(block))
-    #         y_hash_midle.append(block)
-        # if 10000 < contador < 20000:
-        #     x_height_first_20k.append(block_list.index(block))
-        #     y_hash_first_20k.append(block)
-        # elif 40000 >= contador > 20000:
-        #     x_height_middle_20k.append(block_list.index(block))
-        #     y_hash_middle_20k.append(block)
-        # else:
-        #     x_height_last_20k.append(block_list.index(block))
-        #     y_hash_last_20k.append(block)
 
-        # contador += 1
+    for block in block_list:
+        x_height_total.append(block_list.index(block))
+        y_hash_total.append(block)
+        if 50000 < contador < 56000:
+            x_height_middle.append(block_list.index(block))
+            y_hash_midle.append(block)
+        if 10000 < contador < 12000:
+            x_height_first_20k.append(block_list.index(block))
+            y_hash_first_20k.append(block)
+        else:
+            x_height_last_20k.append(block_list.index(block))
+            y_hash_last_20k.append(block)
 
-    # plt.scatter(x=x_height_total, y=y_hash_total, marker='|')
-    # plt.gca().axes.get_yaxis().set_visible(False)
-    # plt.show()
+        contador += 1
+
+    plt.scatter(x=x_height_total, y=y_hash_total, marker='|')
+    plt.gca().axes.get_yaxis().set_visible(False)
+    plt.show()
 
 
 if __name__ == "__main__":
